3319d8d99b844b0f4adb737917f8ee0ddc79d6b9
Bairei <baireikawagishi@gmail.com>
2018-06-12 21:31:21 +0200

    initial commit

---
+++ src/main/kotlin/com/bairei/springjwtkotlindemo/SpringJwtKotlinDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.bairei.springjwtkotlindemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringJwtKotlinDemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringJwtKotlinDemoApplication>(*args)
+}
